be8f8f8646be6697cf7a3310893a72d6295fba10
Lucas SCHNELZAUER <134488249+LucasSch1@users.noreply.github.com>
2023-10-10 08:47:02 +0200

    Add files via upload

---
+++ SCHNELZAUER_Lucas-TD4/Lycée.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SCHNELZAUER_Lucas_TD4
+{
+    class Lycée
+    {
+
+        //Méthode pour lire le fichier Etudiants
+        public static int ReadFile(string fileName)
+        {
+            //Permet de lire un fichier 
+            StreamReader lecteur = new StreamReader(fileName);
+            String line = "";
+            int numberOfChars = 0;
+
+
+            while (line != null)
+            {
+                line = lecteur.ReadLine();
+
+                if (line != null)
+                {
+                    numberOfChars += line.Length;
+                    Console.WriteLine(line);
+                }
+
+            }
+
+            lecteur.Close();
+            return numberOfChars;
+        }
+
+
+        public static void ReadFileEtudiant(string etudiantFile,string enseignantFile)
+        {
+            int TotalEtudiants = 0;
+            int TotalEnseignants = 0;
+            int TotalGlobal = 0;
+
+            // Lire le fichier des étudiants
+            using (StreamReader lecteurEtudiant = new StreamReader(etudiantFile))
+            {
+                string line = "";
+                while ((line = lecteurEtudiant.ReadLine()) != null)
+                {
+                    // Séparer chaque partie en utilisant les tabulations du fichier Etudiant.txt
+                    string[] parts = line.Split('\t');
+                    if (parts.Length >= 5)
+                    {
+                        string Section = parts[4].Trim();
+                        // Incrémente le nombre total d'étudiants
+                        TotalEtudiants++;
+                    }
+                }
+            }
+
+            // Lire le fichier des enseignants
+            using (StreamReader lecteurEnseignant = new StreamReader(enseignantFile))
+            {
+                string line = "";
+                while ((line = lecteurEnseignant.ReadLine()) != null)
+                {
+                    // Séparer chaque partie en utilisant les tabulations du fichier Etudiant.txt
+                    string[] parts = line.Split('\t');
+                    if (parts.Length <= 3)
+                    {
+                       
+                        // Incrémente le nombre total d'étudiants
+                        TotalEnseignants++;
+                    }
+                }
+            }
+            TotalGlobal = TotalEnseignants + TotalEtudiants;
+
+            Console.WriteLine($"Il y a {TotalEtudiants} étudiants ce qui représente {((double)TotalEtudiants / TotalGlobal * 100):F2}% des membres total");
+            Console.WriteLine($"Il y a {TotalEnseignants} enseignants ce qui représente {((double)TotalEnseignants / TotalGlobal * 100):F2}% des membres total");
+            Console.WriteLine($"Il y a  {TotalEtudiants} étudiants + {TotalEnseignants} enseignants ce qui nous fait {TotalGlobal} membres en total");
+
+
+        }
+
+
+        public static void ReadFileEnseignant(string fileName)
+        {
+            //Permet de lire un fichier 
+            StreamReader lecteur = new StreamReader(fileName);
+            String line = "";
+            int TotalEnseignants  = 0;
+
+            while (line != null)
+            {
+                line = lecteur.ReadLine();
+
+                if (line != null)
+                {
+                    // Séparer chaque partie en utilisant les tabulations du fichier Etudiant.txt
+                    string[] parts = line.Split('\t');
+
+                    // Si les parts ont une longueur supérieur ou égale a 4 alors
+                    if (parts.Length >= 5)
+                    {
+                        string Section = parts[4].Trim();
+                        // Incrémente le nombre totale d'étudiants
+                        TotalEnseignants++;
+                    }
+                }
+            }
+
+            lecteur.Close();
+        }
+
+        public void ListerMembres()
+        {
+            Console.WriteLine("Liste des étudiants :");
+            ReadFile("Etudiants.txt");
+            Console.WriteLine("\nListe des professeurs :");
+            ReadFile("Enseignants.txt");
+        }
+
+
+
+
+    }
+}
---
+++ SCHNELZAUER_Lucas-TD4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SCHNELZAUER_Lucas_TD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lycée lycée = new Lycée();
+            
+            //Afficher les choix à l'utilisateur 
+            ColorBlue();
+            Console.WriteLine("Veuillez sélectionner votre choix : ");
+            resetColor();
+            Console.WriteLine("(1) Lister les renseignements du fichier");
+            Console.WriteLine("(2) Lister les renseignements du fichier professeurs ");
+            Console.WriteLine("(3) Lister les renseignements de l’ensemble des membres du lycée ");
+            Console.WriteLine("(4) Calcul des statistiques ");
+            resetColor();
+            //Attribution de la valeur de l'utilisateur à la variable choix de type chaine
+            string choix = Console.ReadLine();
+            //Convertir la chaine en entier
+            Int32 choixutilisateur = Convert.ToInt32(choix);
+
+
+
+            switch (choixutilisateur)
+            {
+                case 1:
+                    ColorBlue();
+                    Console.WriteLine("Voici la liste du fichier Etudiants :");
+                    resetColor();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    //Déclaration du fichier Etudiant en utilisation la Méthode ReadFile  
+                    Lycée.ReadFile("Etudiants.txt");
+                    break; // Le "break" est utilisé pour sortir du switch une fois l'option traitée
+
+
+                case 2:
+                    ColorBlue();
+                    Console.WriteLine("Voici la liste du fichier professeurs :");
+                    resetColor();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    //Déclaration du fichier Etudiant en utilisation la Méthode ReadFile
+                    Lycée.ReadFile("Enseignants.txt");
+
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("Lister les renseignements de l’ensemble des membres du lycée :");
+                    lycée.ListerMembres();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Calcul des statistiques :");
+                    Lycée.ReadFileEtudiant("Etudiants.txt","Enseignants.txt");
+                    break;
+
+
+                    
+                default:
+                    Console.WriteLine("Choix non reconnu.");
+                    // Code  exécuter si aucune des options ne correspond à la valeur de choix
+                    break;
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+       
+
+       
+
+
+        //Méthode qui va réinitialiser la couleur d'origine du texte (blanc)
+        private static void resetColor()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        //Méthode qui va attribuer la couleur bleu au texte
+        private static void ColorBlue()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+    }
+}
